bf58296bb039033f31543de153074f6e8d968327
Ashish Singh <ashishsingh2b@gmail.com>
2024-06-20 10:48:36 +0530

    second commit

--- trans.py
+++
@@ -1,6 +0,0 @@
-from googletrans import Translator
-
-def translate_text(input_text, source_language, target_language):
-    translator = Translator()
-    translated_text = translator.translate(input_text, src=source_language, dest=target_language).text
-    return translated_text
